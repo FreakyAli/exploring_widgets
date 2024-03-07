ed1bf3b384d7e63cb921a8cddf3ab02b12fd0eb2
Gulam Ali H <31090457+FreakyAli@users.noreply.github.com>
2024-03-08 03:21:42 +0530

    fix: initial commit with buttons

---
+++ exploring_widgets/lib/initializers/main.dart
@@ -0,0 +1,6 @@
+import 'package:exploring_widgets/initializers/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ exploring_widgets/lib/pages/buttons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ButtonsPage extends StatefulWidget {
+  const ButtonsPage({super.key});
+
+  @override
+  State<ButtonsPage> createState() => _ButtonsPageState();
+}
+
+class _ButtonsPageState extends State<ButtonsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
